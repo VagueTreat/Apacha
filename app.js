9becc6bc291c09fdabc71f88aadaba35b65b447a
唐唐 <475481415@qq.com>
2018-08-07 20:15:13 +0800

    判断文件是否存在

--- app.js
+++ app.js
@@ -11,7 +11,29 @@ let rootPath = path.join(__dirname,'www');//console.log(rootPath);
 
 //创建服务器
 let server = http.createServer((request,response)=>{
-    response.end('hello')
+    //生成地址
+    let targetPath = path.join(rootPath,request.url);//console.log(targetPath);
+    //1,判断路径是否存在
+    if(fs.existsSync(targetPath)){
+        
+    }else{
+        //转义
+        response.setHeader('content-type','text/html;charset=utf-8');
+        //如果没有这个文件,把状态码也改成404
+        response.statusCode = 404;
+        response.end(`
+            <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+            <html><head>
+            <title>404 Not Found</title>
+            </head><body>
+            <h1>Not Found</h1>
+            <p>你请求的${request.url} 不在服务器上哦,检查一下呗</p>
+            </body></html>
+            `)
+    }
+    //2,判断是文件夹还是文件
+    //3,判断文件的后缀名,
+    //4,跳转
 });
 
 server.listen(3000,'127.0.0.1',()=>{
